051bf87cd0fc161d102e0e99194bf0d1377f89b7
mbright90 <michael.bright90@icloud.com>
2023-01-18 10:15:29 +0000

    Update dependencies and environment

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+}
