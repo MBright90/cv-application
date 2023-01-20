708eddc87a6747dd161d097c4b122ed6dc7e6837
mbright90 <michael.bright90@icloud.com>
2023-01-20 10:57:10 +0000

    Implement You component and conditional rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 
 import Header from './Header'
+import You from './You'
+
+import Server from '../modules/Server'
+const server = new Server()
 
 export default class App extends Component {
   constructor(props) {
@@ -18,11 +22,16 @@ export default class App extends Component {
   }
 
   render() {
+    const mainPage = this.state.currentPage
+    let main
+
+    if (mainPage === 'you') main = <You userInfo={server.getCurrentInfo()}/>
+
     return (
       <div className="page-layout">
         <Header changePageShown={this.changePageShown}/>
+        {main}
       </div>
     )
   }
-    
 }
