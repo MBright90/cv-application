655380945bd741263ee9d76cf123002893e7179f
mbright90 <michael.bright90@icloud.com>
2023-01-21 13:39:14 +0000

    Initialise and implemement Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-link">
+        <a href="https://github.com/MBright90/cv-application"><i className="fa-brands fa-github"></i>MBright90</a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
