f03650dfe5ff3e184607671315269ae208c75af3
mbright90 <michael.bright90@icloud.com>
2023-01-22 13:37:49 +0000

    Initiate and add skeleton funcs to Education component

---
+++ src/components/Education.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+const EducationDetail = (props) => {
+
+}
+
+const EducationList = (props) => {
+
+}
+
+const EducationInput = (props) => {
+    
+}
+
+export default class Education extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <main>
+        <div className='education-page-overview'>
+          <EducationList />
+        </div>
+      </main>
+    )
+  }
+}
