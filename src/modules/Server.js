3c2e1ed8286da770bd5910f6eef4c70e2b88833b
mbright90 <michael.bright90@icloud.com>
2023-01-22 11:49:45 +0000

    Add functions to save to local Storage

--- src/modules/Server.js
+++ src/modules/Server.js
@@ -2,20 +2,39 @@ export default class Server {
   constructor() {
     this.user = {
       name: '',
+      email: '',
       contactNumber: '',
       avatarImg: null,
       experience: null,
       education: null,
       coverLetter: null,
     }
+
+    const savedUser = localStorage.getItem('user')
   }
 
   getCurrentInfo() {
     return this.user
   }
 
+  saveToStorage() {
+    localStorage.setItem('user', this.user)
+  }
+
   saveAvatarChange(newAvatarImage) {
     this.user.avatarImg = newAvatarImage
+  }
+
+  saveAccountInfo(infoArray) {
+    this.user.name = infoArray.name !== '' 
+      ? infoArray.name 
+      : this.user.name
+    this.user.email = infoArray.email !== ''
+      ? infoArray.email
+      : this.user.email
+    this.user.contactNumber = infoArray.number !== ''
+      ? infoArray.contactNumber
+      : this.user.contactNumber
   }
 
   // Validation methods
