084af1cd54709aa3d66416bba215e56894e98805
mbright90 <michael.bright90@icloud.com>
2023-01-24 20:17:39 +0000

    Add functionality to input fields and display

--- src/components/Education.js
+++ src/components/Education.js
@@ -4,12 +4,22 @@ import PropTypes from 'prop-types'
 import SaveButton from './SaveButton'
 
 const EducationDetail = (props) => {
-  const education = props.educationItem
+  const certificateParaArray = (certificateArray) => {
+    return certificateArray.map((certificate) => {
+      return (
+        <p
+          key={certificate}
+          className="education-item-certificate"
+        >{certificate}</p>
+      )
+    })
+  }
 
   return (
     <div className="education-list-item">
-      <p className="education-detail-headline">{education.institutionName} {education.dates}</p>
-      <p>{education.certificateAwarded}</p>
+      <p className="education-item-headline">{props.educationItem.institutionName.toUpperCase()}</p>
+      <p className="education-item-dates">{props.educationItem.dateFrom} - {props.educationItem.dateTo}</p>
+      {certificateParaArray(props.educationItem.certificates)}
     </div>
   )
 }
@@ -20,10 +30,10 @@ EducationDetail.propTypes = {
 
 const EducationList = (props) => {
   const createEducationList = () => {
-    props.educationArray.map((educationItem) => {
+    return props.educationArray.map((educationItem) => {
       return <EducationDetail
-        key={educationItem.institutionName + educationItem.dates}
-        educationDetail={educationItem}/>
+        key={`${educationItem.institutionName}${educationItem.dateFrom}`}
+        educationItem={educationItem}/>
     })
   }
 
@@ -38,55 +48,120 @@ EducationList.propTypes = {
   educationArray: PropTypes.array
 }
 
-const EducationInput = (props) => {
-  // education: institutionName, date from, date to, certificate awarded
-
+const CertificateInput = (props) => {
   return (
-    <form className='education-input-overview'>
-      <fieldset>
-        <legend>Add New Certificate</legend>
-        <div>
-          <label htmlFor="">Institution</label>
-          <input 
-            type="text"
-            id=""
-            defaultValue={props.educationItem.institutionName}/>
-        </div>
-        <div>
-          <label htmlFor="">Certificate</label>
-          <input 
-            type="text"
-            id=""
-            defaultValue={props.educationItem.certificate}
-          />
-        </div>
-        <div>
-          <label htmlFor="">Date From</label>
-          <input 
-            type="date"
-            id=""
-            defaultValue={props.educationItem.dateFrom}
-          />
-        </div>
-        <div>
-          <label htmlFor="">Date To</label>
-          <input 
-            type="date" 
-            id=""
-            defaultValue={props.educationItem.dateTo}
-          />
-        </div>
-        <SaveButton 
-          uploadData={props.uploadEducationInfo}/>
-      </fieldset>
-    </form>
+    <input
+      className="certificate-input"
+      type="text"
+      id={`certificate${props.inputIndex}`}
+      defaultValue={props.currentCertificate}
+      data-input-index={props.inputIndex}
+      onChange={props.handleCertificateChange}
+    />
   )
+}
+
+CertificateInput.propTypes = {
+  currentCertificate: PropTypes.string,
+  handleCertificateChange: PropTypes.func,
+  inputIndex: PropTypes.number,
+}
+
+class EducationInput extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      certificateInputAmount: this.props.educationItem.certificates.length,
+      currentCertificateValues: [...this.props.educationItem.certificates]
+    }
+
+    this.handleCertificateChange = this.handleCertificateChange.bind(this)
+    this.handleNewCertificate = this.handleNewCertificate.bind(this)
+  }
+
+  handleCertificateChange(e) {
+    const certificateIndex = parseInt(e.target.dataset.inputIndex, 10)
+    const newCertificateValues = [...this.state.currentCertificateValues]
+    newCertificateValues[certificateIndex] = e.target.value
+
+    this.setState({
+      currentCertificateValues: newCertificateValues
+    })
+  }
+
+  handleNewCertificate() {
+    this.setState({
+      certificateInputAmount: this.state.certificateInputAmount + 1
+    })
+  }
+
+  createCertificateInputs = () => {
+    // Create an array of n from certificateInputAmount
+    const indexArray = [...Array(this.state.certificateInputAmount).keys()]
+
+    // Map out array using each index to create a certificate input
+    return indexArray.map((index) => {
+      return (<CertificateInput 
+        key={`certificate-${index}`}
+        inputIndex={index}
+        currentCertificate={this.state.currentCertificateValues[index]}
+        handleCertificateChange={this.handleCertificateChange}
+      />
+      )
+    })
+  }
+
+  render() {
+    return (
+      <form className='education-input-overview'>
+        <fieldset>
+          <legend>Add New Certificate</legend>
+          <div>
+            <label htmlFor="">Institution</label>
+            <input 
+              type="text"
+              id=""
+              defaultValue={this.props.educationItem.institutionName}/>
+          </div>
+          <div>
+            <label htmlFor="">Date From</label>
+            <input 
+              type="date"
+              id=""
+              defaultValue={this.props.educationItem.dateFrom}
+            />
+          </div>
+          <div>
+            <label htmlFor="">Date To</label>
+            <input 
+              type="date" 
+              id=""
+              defaultValue={this.props.educationItem.dateTo}
+            />
+          </div>
+          <div className="certificate-input-container">
+            <label htmlFor="certificates">Certificate(s)</label>
+            {this.createCertificateInputs()}
+            <button
+              className="new-certificate-button"
+              type="button"
+              onClick={this.handleNewCertificate}>
+              <i className="fa-solid fa-plus"></i>
+            </button>
+          </div>
+          <SaveButton 
+            uploadData={this.props.uploadEducationInfo}/>
+        </fieldset>
+      </form>
+    )
+  }
 }
 
 EducationInput.defaultProps = {
   educationItem: {
     institutionName: '',
-    certificate: '',
+    certificates: [''],
     dateFrom: new Date(1, 1, 1970),
     dateTo: new Date(1, 1, 1971),
   }
