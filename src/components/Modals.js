0a6707472c499089d0b39d401cb0d2bd4d9e6967
mbright90 <michael.bright90@icloud.com>
2023-01-28 09:29:50 +0000

    Refactor element structure of EditInfoModal

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -1,32 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { EducationInput } from './Education'
-import { ExperienceInput } from './Experience'
-
 const DeleteInfoModal = () => {
   console.log('delete')
 }
 
 const EditInfoModal = (props) => {
-
-  //   const editInfoType = props.typeOfInfo
-  //   let editForm
-  
-  //   if (editInfoType =='education') editForm = <EducationInput />
-  //   else editForm = <ExperienceInput />
-
-  const editForm = props.editForm
-
   return (
     <div className="modal">
-      {editForm}
+      <div className="modal-form-container">
+        {props.editForm}
+      </div>
     </div>
   )
 }
 
 EditInfoModal.propTypes = {
-  editForm: PropTypes.string,
+  editForm: PropTypes.object,
 }
 
 export { EditInfoModal, DeleteInfoModal }
