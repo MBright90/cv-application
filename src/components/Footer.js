0ce7844a004dc2c3cc1077dc54a134caf574b299
mbright90 <michael.bright90@icloud.com>
2023-02-06 11:44:52 +0000

    Remove reset functions from within footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,13 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
-import { ResetInfoModal } from './Modals'
-
-const Footer = (props) => {
+const Footer = () => {
   return (
     <footer>
       <div className="footer-link">
         <a href="https://github.com/MBright90/cv-application"><i className="fa-brands fa-github"></i>MBright90</a>
       </div>
-      <ResetInfoModal 
-        closeModal={props.closeModal}
-        resetFunc={props.resetFunc}/>
     </footer>
   )
-}
-
-Footer.propTypes = {
-  closeModal: PropTypes.func,
-  resetFunc: PropTypes.func,
 }
 
 export default Footer
