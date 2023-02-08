86b76af61d0cbb8d46cbae98ee349a752455e788
mbright90 <michael.bright90@icloud.com>
2023-02-08 16:14:05 +0000

    Restructure files and related imports

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 
 import App from './components/App'
 
-import './assets/style/style.css'
+import './assets/style.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 
 const root = ReactDOM.createRoot(document.getElementById('app'))
