02954bb05d0873bc549d3cf5167cc7549d823ca7
mbright90 <michael.bright90@icloud.com>
2023-02-08 15:16:27 +0000

    Restructure import positions

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 
 import App from './components/App'
 
+import './assets/style/style.css'
 import '@fortawesome/fontawesome-free/css/all.css'
-import './style/style.css'
 
 const root = ReactDOM.createRoot(document.getElementById('app'))
 root.render(<App />)
