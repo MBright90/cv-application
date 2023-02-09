754b7f89679b40626aa51b8d42fe13feca9c911c
mbright90 <michael.bright90@icloud.com>
2023-02-09 15:20:39 +0000

    Create additional directories and current reposition files

---
+++ src/components/utilities/Buttons.js
@@ -0,0 +1,164 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons'
+
+class SaveInfoButton extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      currentMessage: '',
+    }
+  
+    this.notifyMessage = this.notifyMessage.bind(this)
+  }
+
+  #removeInvalidHighlight = (inputElement) => {
+    // Clear the success notification after three seconds
+    this.highlightTimerID = setTimeout(() => {
+      inputElement.classList.remove('invalid-highlight')
+    }, 3000)
+  }
+  
+  highlightInvalidField = (inputElement) => {
+    inputElement.classList.add('invalid-highlight')
+    this.#removeInvalidHighlight(inputElement)
+  }
+  
+  #clearNotification = () => {
+    // Clear the success notification after three seconds
+    this.notificationTimerID = setTimeout(() => {
+      this.setState({
+        currentMessage: ''
+      })
+    }, 3000)
+  }
+  
+  notifyMessage = (message) => {
+    this.setState({
+      currentMessage: message
+    })
+    this.#clearNotification()
+  }
+  
+  handleFormData = (e) => {
+    const clearValues = (inputElements) => {
+      inputElements.forEach((el) => el.value = null)
+    }
+
+    e.preventDefault()
+
+    // Search for all inputs/textAreas within the form and 
+    // submit to the server for validation before committing
+    const inputArr = [
+      ...e.target.parentNode.parentNode.querySelectorAll('input'),
+      ...e.target.parentNode.parentNode.querySelectorAll('textarea')
+    ]
+
+    // Retrieve an array, with index 0 being the validation message and
+    // index 1 being the first invalid field
+    const validatedInfo = this.props.validateInputSubmission(inputArr)
+
+    if (!validatedInfo[1]) {
+      // map into an array of values for parsing and pass to relevant server function
+      const inputValues = inputArr.map((inputField) => inputField.value)
+      this.props.uploadData(inputValues, e.target.dataset.itemId, e.target.dataset.infoType)
+      this.notifyMessage('Update successful')
+      if (this.props.setToClear === true) clearValues(inputArr)
+
+      // If editing via an open modal, close model after submitting
+      if (this.props.closeModal) this.props.closeModal()
+
+    } else {
+      // If there is a validation message to display, notify the user and
+      // highlight all invalid fields
+      this.notifyMessage(validatedInfo[0])
+      this.highlightInvalidField(validatedInfo[1])
+    }
+
+  }
+  
+  render() {
+    return (
+      <div className="save-button-container span-two">
+        <button 
+          className="save-button hover-button"
+          type="submit"
+          data-item-id={this.props.itemID}
+          data-info-type={this.props.infoType}
+          onClick={this.handleFormData}
+        >Update</button>
+        <p>{this.state.currentMessage}</p>
+      </div>
+    )
+  }
+}
+
+SaveInfoButton.defaultProps = {
+  infoType: '',
+  itemID: '',
+  setToClear: true
+}
+
+SaveInfoButton.propTypes = {
+  closeModal: PropTypes.func,
+  infoType: PropTypes.string,
+  itemID: PropTypes.string,
+  setToClear: PropTypes.bool,
+  uploadData: PropTypes.func,
+  validateInputSubmission: PropTypes.func,
+}
+
+const EditButton = (props) => {
+  return (
+    <button
+      className="edit-button hover-button"
+      type="button"
+      data-item-id={props.itemID}
+      onClick={props.editFunc}>
+      <FontAwesomeIcon icon={faPenToSquare} />
+    </button>
+  )
+}
+
+EditButton.propTypes = {
+  editFunc: PropTypes.func,
+  itemID: PropTypes.string,
+}
+
+const DeleteButton = (props) => {
+  return (
+    <button
+      className="delete-button hover-button"
+      type="button"
+      data-item-id={props.itemID}
+      onClick={props.showDeleteFunc}>
+      <FontAwesomeIcon icon={faTrashCan} />
+    </button>
+  )
+}
+
+DeleteButton.propTypes = {
+  itemID: PropTypes.string,
+  showDeleteFunc: PropTypes.func,
+}
+
+const ResetButton = (props) => {
+
+  return (
+    <button
+      className="reset-data-button"
+      type="button"
+      onClick={props.showResetModal}>
+          Reset Site Data
+    </button>
+  )
+}
+
+ResetButton.propTypes = {
+  showResetModal: PropTypes.func,
+}
+
+export { SaveInfoButton, EditButton, DeleteButton, ResetButton }
---
+++ src/components/utilities/Modals.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ModalCloseButton = (props) => {
+  return (
+    <button 
+      className="modal-close-button"
+      onClick={props.closeModal}>
+      <i className="fa-solid fa-x"></i>
+    </button>
+  )
+}
+
+ModalCloseButton.propTypes = {
+  closeModal: PropTypes.func,
+}
+
+const EditInfoModal = (props) => {
+  return (
+    <div className="modal">
+      <div className="modal-form-container">
+        {props.editForm}
+        <ModalCloseButton closeModal={props.closeModal} />
+      </div>
+    </div>
+  )
+}
+
+EditInfoModal.propTypes = {
+  closeModal: PropTypes.func,
+  editForm: PropTypes.object,
+}
+
+const DeleteInfoModal = (props) => {
+
+  const handleDeleteClick = (e) => {
+    const infoID = e.target.dataset.itemId
+    const infoType = e.target.dataset.type
+
+    props.deleteFunc(infoID, infoType)
+    props.closeModal()
+  }
+
+  return (
+    <div className="modal">
+      <div className="modal-form-container">
+        <div className="confirm-container">
+          <p>Are you sure you want to delete this?</p>
+          <button
+            data-item-id={props.itemID}
+            data-type={props.type}
+            onClick={handleDeleteClick}
+          >Confirm</button>
+        </div>
+        <ModalCloseButton closeModal={props.closeModal} />
+      </div>
+    </div>
+  )
+}
+
+DeleteInfoModal.propTypes = {
+  closeModal: PropTypes.func,
+  deleteFunc: PropTypes.func,
+  itemID: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+  ]),
+  type: PropTypes.string,
+}
+
+const ResetInfoModal = (props) => {
+
+  const handleResetClick = () => {
+    props.resetFunc()
+    props.closeModal()
+  }
+
+  return (
+    <div className="modal">
+      <div className="modal-form-container">
+        <div className="confirm-container">
+          <p>This action cannot be reversed. Are you sure you wish to clear all data?</p>
+          <button
+            onClick={handleResetClick}
+          >Confirm</button>
+        </div>
+        <ModalCloseButton closeModal={props.closeModal}/>
+      </div>
+    </div>
+  )
+}
+
+ResetInfoModal.propTypes = {
+  closeModal: PropTypes.func,
+  resetFunc: PropTypes.func,
+}
+
+export { EditInfoModal, DeleteInfoModal, ResetInfoModal }
