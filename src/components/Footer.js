377eb64f79b34c23d9bba0c7a90f326884882bc6
mbright90 <michael.bright90@icloud.com>
2023-02-09 12:44:02 +0000

    Add font-awesome icon to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
 const Footer = () => {
   return (
     <footer>
       <div className="footer-link">
-        <a href="https://github.com/MBright90/cv-application"><i className="fa-brands fa-github"></i>MBright90</a>
+        <a href="https://github.com/MBright90/cv-application"><FontAwesomeIcon icon={faGithub} size="lg"/> MBright90</a>
       </div>
     </footer>
   )
