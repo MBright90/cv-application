58d110c52d0f7a60b75462cc0a5d8e9169919d66
mbright90 <michael.bright90@icloud.com>
2023-02-15 10:39:34 +0000

    Convert EducationForm to use hooks

--- src/components/education/educationForm/EducationForm.js
+++ src/components/education/educationForm/EducationForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import './style.css'
@@ -27,23 +27,13 @@ CertificateInput.propTypes = {
   inputIndex: PropTypes.number
 }
 
-export default class EducationForm extends Component {
-  constructor(props) {
-    super(props)
+export default function EducationForm(props) {
+  const [certificateInputAmount, setCertificateInputAmount] = useState(props.educationItem.certificates.length)
+  const currentCertificateValues = [...props.educationItem.certificates]
 
-    this.state = {
-      certificateInputAmount: this.props.educationItem.certificates.length,
-      currentCertificateValues: [...this.props.educationItem.certificates]
-    }
-
-    this.handleInfoSave = this.handleInfoSave.bind(this)
-    this.handleNewCertificate = this.handleNewCertificate.bind(this)
-    this.handleValueChange = this.handleValueChange.bind(this)
-  }
-
-  createCertificateInputs = () => {
+  const createCertificateInputs = () => {
     // Create an array of n from certificateInputAmount
-    const indexArray = [...Array(this.state.certificateInputAmount).keys()]
+    const indexArray = [...Array(certificateInputAmount).keys()]
 
     // Map out array using each index to create a certificate input
     return indexArray.map((index) => {
@@ -51,96 +41,88 @@ export default class EducationForm extends Component {
         <CertificateInput
           key={`certificate-${index}`}
           inputIndex={index}
-          currentCertificate={this.state.currentCertificateValues[index]}
-          handleValueChange={this.handleValueChange}
+          currentCertificate={currentCertificateValues[index]}
+          handleValueChange={handleValueChange}
         />
       )
     })
   }
 
-  handleInfoSave(inputValues, infoID, infoType) {
-    this.setState({
-      certificateInputAmount: 1
-    })
-    this.props.uploadEducationInfo(inputValues, infoID, infoType)
+  const handleInfoSave = (inputValues, infoID, infoType) => {
+    setCertificateInputAmount(1)
+    props.uploadEducationInfo(inputValues, infoID, infoType)
   }
 
-  handleNewCertificate() {
-    this.setState({
-      certificateInputAmount: this.state.certificateInputAmount + 1
-    })
+  const handleNewCertificate = () => {
+    setCertificateInputAmount(certificateInputAmount + 1)
   }
 
-  handleValueChange(e) {
-    this.props.validateInput(e.target)
+  const handleValueChange = (e) => {
+    props.validateInput(e.target)
   }
 
-  render() {
-    let closeModal
-    if (this.props.closeModal) closeModal = this.props.closeModal
-
-    return (
-      <form className="education-input-overview">
-        <fieldset>
-          <legend>{this.props.formType} New Certificate</legend>
-          <div className="span-two">
-            <label htmlFor="">Institution</label>
-            <input
-              type="text"
-              id="education-institution-input"
-              minLength="3"
-              onChange={this.handleValueChange}
-              data-is-required={true}
-              defaultValue={this.props.educationItem.institutionName}
-            />
-          </div>
-          <div>
-            <label htmlFor="">Date From</label>
-            <input
-              type="date"
-              id="education-date-from-input"
-              onChange={this.handleValueChange}
-              data-is-required={true}
-              data-date="from"
-              defaultValue={this.props.educationItem.dateFrom}
-            />
-          </div>
-          <div>
-            <label htmlFor="">Date To</label>
-            <input
-              type="date"
-              id="education-date-to-input"
-              onChange={this.handleValueChange}
-              data-is-required={true}
-              data-date="to"
-              defaultValue={this.props.educationItem.dateTo}
-            />
-          </div>
-          <div className="certificate-input-container span-two">
-            <label htmlFor="certificates">Certificate(s)</label>
-            {this.createCertificateInputs()}
-            <button
-              className="new-certificate-button hover-button"
-              type="button"
-              onClick={this.handleNewCertificate}
-            >
-              <FontAwesomeIcon icon={faPlus} />
-            </button>
-          </div>
-          <SaveInfoButton
-            closeModal={closeModal}
-            itemID={this.props.itemID}
-            infoType="education"
-            uploadData={this.handleInfoSave}
-            validateInputSubmission={this.props.validateInputSubmission}
+  return (
+    <form className="education-input-overview">
+      <fieldset>
+        <legend>{props.formType} New Certificate</legend>
+        <div className="span-two">
+          <label htmlFor="">Institution</label>
+          <input
+            type="text"
+            id="education-institution-input"
+            minLength="3"
+            onChange={handleValueChange}
+            data-is-required={true}
+            defaultValue={props.educationItem.institutionName}
           />
-        </fieldset>
-      </form>
-    )
-  }
+        </div>
+        <div>
+          <label htmlFor="">Date From</label>
+          <input
+            type="date"
+            id="education-date-from-input"
+            onChange={handleValueChange}
+            data-is-required={true}
+            data-date="from"
+            defaultValue={props.educationItem.dateFrom}
+          />
+        </div>
+        <div>
+          <label htmlFor="">Date To</label>
+          <input
+            type="date"
+            id="education-date-to-input"
+            onChange={handleValueChange}
+            data-is-required={true}
+            data-date="to"
+            defaultValue={props.educationItem.dateTo}
+          />
+        </div>
+        <div className="certificate-input-container span-two">
+          <label htmlFor="certificates">Certificate(s)</label>
+          {createCertificateInputs()}
+          <button
+            className="new-certificate-button hover-button"
+            type="button"
+            onClick={handleNewCertificate}
+          >
+            <FontAwesomeIcon icon={faPlus} />
+          </button>
+        </div>
+        <SaveInfoButton
+          closeModal={props.closeModal}
+          itemID={props.itemID}
+          infoType="education"
+          uploadData={handleInfoSave}
+          validateInputSubmission={props.validateInputSubmission}
+        />
+      </fieldset>
+    </form>
+  )
 }
 
 EducationForm.defaultProps = {
+  closeModal: () => {},
   educationItem: {
     institutionName: '',
     dateFrom: new Date(1970).toISOString().substring(0, 10),
