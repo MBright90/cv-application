286c6964de3f4e8da5a1bf8a59ba283c877b6e19
mbright90 <michael.bright90@icloud.com>
2023-02-17 09:57:34 +0000

    Convert SaveInfoButton to functional component

--- src/components/utilities/buttons/saveInfoButton/SaveInfoButton.js
+++ src/components/utilities/buttons/saveInfoButton/SaveInfoButton.js
@@ -1,48 +1,39 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 
 import './style.css'
 
-export default class SaveInfoButton extends Component {
-  constructor(props) {
-    super(props)
+export default function SaveInfoButton(props) {
+  const [activeMessage, setActiveMessage] = useState('')
 
-    this.state = {
-      currentMessage: ''
-    }
+  let highlightTimerID
+  let notificationTimerID
 
-    this.notifyMessage = this.notifyMessage.bind(this)
-  }
-
-  #removeInvalidHighlight = (inputElement) => {
-    // Clear the success notification after three seconds
-    this.highlightTimerID = setTimeout(() => {
+  const removeInvalidHighlight = (inputElement) => {
+    // Clear the invalid highlighting after three seconds
+    highlightTimerID = setTimeout(() => {
       inputElement.classList.remove('invalid-highlight')
     }, 3000)
   }
 
-  highlightInvalidField = (inputElement) => {
+  const highlightInvalidField = (inputElement) => {
     inputElement.classList.add('invalid-highlight')
-    this.#removeInvalidHighlight(inputElement)
+    removeInvalidHighlight(inputElement)
   }
 
-  #clearNotification = () => {
+  const clearNotification = () => {
     // Clear the success notification after three seconds
-    this.notificationTimerID = setTimeout(() => {
-      this.setState({
-        currentMessage: ''
-      })
+    notificationTimerID = setTimeout(() => {
+      setActiveMessage('')
     }, 3000)
   }
 
-  notifyMessage = (message) => {
-    this.setState({
-      currentMessage: message
-    })
-    this.#clearNotification()
+  const notifyMessage = (message) => {
+    setActiveMessage(message)
+    clearNotification()
   }
 
-  handleFormData = (e) => {
+  const handleFormData = (e) => {
     const clearValues = (inputElements) => {
       inputElements.forEach((el) => (el.value = null))
     }
@@ -58,47 +49,46 @@ export default class SaveInfoButton extends Component {
 
     // Retrieve an array, with index 0 being the validation message and
     // index 1 being the first invalid field
-    const validatedInfo = this.props.validateInputSubmission(inputArr)
+    const validatedInfo = props.validateInputSubmission(inputArr)
 
     if (!validatedInfo[1]) {
       // map into an array of values for parsing and pass to relevant server function
       const inputValues = inputArr.map((inputField) => inputField.value)
-      this.props.uploadData(inputValues, e.target.dataset.itemId, e.target.dataset.infoType)
-      this.notifyMessage('Update successful')
-      if (this.props.setToClear === true) clearValues(inputArr)
+      props.uploadData(inputValues, e.target.dataset.itemId, e.target.dataset.infoType)
+      notifyMessage('Update successful')
+      if (props.setToClear === true) clearValues(inputArr)
 
       // If editing via an open modal, close model after submitting
-      if (this.props.closeModal) this.props.closeModal()
+      if (props.closeModal) props.closeModal()
     } else {
       // If there is a validation message to display, notify the user and
       // highlight all invalid fields
-      this.notifyMessage(validatedInfo[0])
-      this.highlightInvalidField(validatedInfo[1])
+      notifyMessage(validatedInfo[0])
+      highlightInvalidField(validatedInfo[1])
     }
   }
 
-  componentWillUnmount() {
-    // Clear any current timers if user changes tabs
-    clearTimeout(this.highlightTimerID)
-    clearTimeout(this.notificationTimerID)
-  }
+  useEffect(() => {
+    return () => {
+      clearTimeout(highlightTimerID)
+      clearTimeout(notificationTimerID)
+    }
+  }, [])
 
-  render() {
-    return (
-      <div className="save-button-container span-two">
-        <button
-          className="save-button hover-button"
-          type="submit"
-          data-item-id={this.props.itemID}
-          data-info-type={this.props.infoType}
-          onClick={this.handleFormData}
-        >
-          Update
-        </button>
-        <p>{this.state.currentMessage}</p>
-      </div>
-    )
-  }
+  return (
+    <div className="save-button-container span-two">
+      <button
+        className="save-button hover-button"
+        type="submit"
+        data-item-id={props.itemID}
+        data-info-type={props.infoType}
+        onClick={handleFormData}
+      >
+        Update
+      </button>
+      <p>{activeMessage}</p>
+    </div>
+  )
 }
 
 SaveInfoButton.defaultProps = {
