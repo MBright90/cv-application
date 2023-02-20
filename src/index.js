3246338b1533f021b269dbd071edc55480015250
mbright90 <michael.bright90@icloud.com>
2023-02-20 11:02:13 +0000

    Refactor imports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './app'
+import { AppProvider } from './app/appContext'
 
 const root = ReactDOM.createRoot(document.getElementById('app'))
-root.render(<App />)
+root.render(<AppProvider />)
