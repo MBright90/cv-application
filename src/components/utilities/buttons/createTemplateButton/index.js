b20b65e2eb04b2544a6851cd68bb6ce7daa13fcc
mbright90 <michael.bright90@icloud.com>
2023-02-20 12:51:33 +0000

    Resolve issue in context imports

--- src/components/utilities/buttons/createTemplateButton/index.js
+++ src/components/utilities/buttons/createTemplateButton/index.js
@@ -1,19 +1,14 @@
-import React, { useContext } from 'react'
-
 import { appContext } from '@app/appContext'
+import React, { useContext } from 'react'
 
 import style from './style.module.css'
 
-export default function CreateTemplateButton(props) {
-
-  const changePageShown = useContext(appContext)
+export default function CreateTemplateButton() {
+  const { changePageShown } = useContext(appContext)
 
   return (
     <div className={style.templateContainer}>
-      <button
-        className={style.createTemplateBtn}
-        onClick={() => changePageShown('cv-template')}
-      >
+      <button className={style.createTemplateBtn} onClick={() => changePageShown('cv-template')}>
         Create Template
       </button>
     </div>
