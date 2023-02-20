369753a9d285708937e688c8eb2d64c65fa8dccf
mbright90 <michael.bright90@icloud.com>
2023-02-20 12:52:57 +0000

    Prettify page

--- src/components/utilities/error/index.js
+++ src/components/utilities/error/index.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSadTear } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
 
 import style from './style.module.css'
 
 export default function ErrorPage() {
-  return(
+  return (
     <main>
       <div className={style.errorPageOverview}>
         <FontAwesomeIcon icon={faSadTear} />
